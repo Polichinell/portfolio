e1750f752a2e516211bdfc787c098231b4a0895f
Polichinell <tsuqinoa@gmail.com>
2023-11-30 20:21:52 +0100

    style(app): test style+ deployment

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,15 @@
 
 export default function Home() {
   return (
-    <main>
-      Hi :D
+    <main className="text-center pt-5">
+      <h1 className="self-center text-6xl text-amber-700">Polichinell</h1>
+
+      <h2 className="text-xl italic text-amber-500">Label | Label | Label</h2>
+
+
+      <div className="bg-white bg-opacity-70 rounded-3xl w-7/12 mx-auto mt-5" style={{height: '200px'}}>
+
+      </div>
     </main>
   )
 }
