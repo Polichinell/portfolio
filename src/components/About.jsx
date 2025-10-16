0d96822cd65d319b46f4f91dc30f60b04f70de6e
aestuu <pokitowokito@gmail.com>
2025-10-16 21:00:33 +0100

    chore: code update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,17 +10,15 @@ function About() {
       <div className="flex gap-10">
         <div className="bg-white bg-opacity-70 box-info rounded-3xl w-3/4 mx-auto mt-[4rem]">
           <p className="p-[3rem] mt-3 text-md">
-            Hello there!~ I&apos;m Louisa, a coding enthusiast with a passion
-            for pixels and a certain interest in simple yet pretty web designs.
+            Hello there!~ I&apos;m Lou, a coding enthusiast with a passion for
+            pixels and a certain interest in simple yet pretty web designs.
             <br />
             <br />
-            As a junior front-end developer, crafting delightful and engaging
-            digital experiences is my main quest. Brewing a special blend of
-            form and function with HTML, CSS, and JavaScript as my special
-            ingredients, I&apos;m on a mission to bring designs to life and add
-            that extra sprinkle of magic to the web. When I&apos;m not typing
-            away, you can find me creating digital illustrations, adding an
-            extra touch of wonder to the experience.
+            As a junior front-end developer, creating fun and engaging digital
+            experiences is what I strive for. Balancing both form and function
+            with HTML, CSS, and JavaScript. When I&apos;m not typing away, you
+            can find me creating digital illustrations or learning new tools and
+            technologies!
             <br />
             <br />
             Let&apos;s build something amazing together. ♪
